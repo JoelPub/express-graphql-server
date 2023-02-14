8b9c9b89035bc0bf56dc8def2cb4b9a76d6734c3
hypothesisobservation <120163230+hypothesisobservation@users.noreply.github.com>
2023-02-14 09:57:31 +0000

    add Login

--- index.js
+++ index.js
@@ -4,37 +4,46 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-type RandomDie {
-  numSides: Int!
-  rollOnce: Int!
-  roll(numRolls: Int!): [Int]
+type user {
+  email: String!
+  username: String!
+  bio: String
+  image: String
+  token:String!
 }
 
 type Query {
-  getDie(numSides: Int): RandomDie
+  Login(email:String!,password:String!): user
 }
 `);
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
+class user {
+  constructor(email) {
+    this.email = email;
+    this.username = email;
+    this.bio = null;
+    this.image = "https://api.realworld.io/images/smiley-cyrus.jpeg";
+    this.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFzZGZAc2FkZi5kc2FmIiwidXNlcm5hbWUiOiJhc2RmQHNhZGYuZHNhZiIsImlhdCI6MTY3NjM2MzgyMSwiZXhwIjoxNjgxNTQ3ODIxfQ.w2RaV25FEJgX1drS0ZZBJsq8gkY1Kgbtpjhhhuv4Gf0";
   }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
+  email() {
+    return this.username
   }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+  username() {
+    return this.username
+  }
+  bio() {
+    return this.bio
+  }
+  image() {
+    return this.image
+  }
+  token() {
+    return this.token
   }
 }
 // The root provides a resolver function for each API endpoint
 var root = {
-  getDie: ({numSides}) => {
-    return new RandomDie(numSides || 6);
+  Login: ({email,password}) => {
+    return new user(email || 'asdf@sadf.dsaf');
   }
 }
 
@@ -50,3 +59,14 @@ app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 // Export the Express API
 module.exports = app;
+/*run on graphql:
+{
+  Login(email: "asdf@sadf.dsaf",password:"asdf") {
+    email 
+  username 
+  bio 
+  image 
+  token 
+  }
+}
+*/
