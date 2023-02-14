9edcde135e55810cfe0a2cb946d55d87fd347a16
hypothesisobservation <120163230+hypothesisobservation@users.noreply.github.com>
2023-02-14 07:47:15 +0000

    update

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express')
+const app = express()
+const port = 80
+const path = require('path');
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+// Render Html File
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'templates/index.html'));
+});
 
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+app.listen(port, () => {
+  // Code.....
+})
