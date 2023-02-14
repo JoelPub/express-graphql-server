20fb158143e6b9bbca7443680a64e11462f9125c
hypothesisobservation <120163230+hypothesisobservation@users.noreply.github.com>
2023-02-14 09:45:49 +0000

    update

--- index.js
+++ index.js
@@ -4,17 +4,41 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+type RandomDie {
+  numSides: Int!
+  rollOnce: Int!
+  roll(numRolls: Int!): [Int]
+}
 
+type Query {
+  getDie(numSides: Int): RandomDie
+}
+`);
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
+}
+
+
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
